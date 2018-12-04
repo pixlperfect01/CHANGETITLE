f4885570f3a4fec7f785d2cb77d2b4ff93ebaa74
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2018-12-04 09:47:24 -0600

    Create text.js

---
+++ text.js
@@ -0,0 +1,26 @@
+function setup(){
+  noCanvas();
+}
+function draw() {
+  background(255);
+  let txt = select("#txt");
+	let att1 = select("#att1");
+	let att2 = select("#att2");
+  let att3 = select("#att3");
+  /*let b = select("#2");
+  let c = select("#3");
+  let d = select("#4");
+  let e = select("#5");
+  let f = select("#6");
+  let g = select("#7");
+  let i = select("#8");
+  */
+  let txt2 = link.value();
+  
+  a.html(drop.value());
+  b.html(link2);
+  c.html(" width=");
+  d.html(w2);
+  f.html("height=");
+  g.html(h2);
+}
